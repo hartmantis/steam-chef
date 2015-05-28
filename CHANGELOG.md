42696674db419e987cc7b67cfab1dcfc64f7ef82
Jonathan Hartman <j@p4nt5.com>
2015-05-27 20:23:25 -0700

    Initial commit; freshly generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Steam Cookbook CHANGELOG
+========================
+
+v0.0.1 (2015-05-27)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'steam'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures steam'
+long_description 'Installs/Configures steam'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
