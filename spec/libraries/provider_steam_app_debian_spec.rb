21c0c64e57b0b223c09fb561b6e48cd921e4e168
Jonathan Hartman <j@p4nt5.com>
2015-12-12 23:57:05 -0800

    Update no-longer-valid use of nil run_contexts

--- spec/libraries/provider_steam_app_debian_spec.rb
+++ spec/libraries/provider_steam_app_debian_spec.rb
@@ -5,8 +5,9 @@ require_relative '../../libraries/provider_steam_app_debian'
 
 describe Chef::Provider::SteamApp::Debian do
   let(:name) { 'default' }
-  let(:new_resource) { Chef::Resource::SteamApp.new(name, nil) }
-  let(:provider) { described_class.new(new_resource, nil) }
+  let(:run_context) { ChefSpec::SoloRunner.new.converge.run_context }
+  let(:new_resource) { Chef::Resource::SteamApp.new(name, run_context) }
+  let(:provider) { described_class.new(new_resource, run_context) }
 
   describe '.provides?' do
     let(:platform) { nil }
