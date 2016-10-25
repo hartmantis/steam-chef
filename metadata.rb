092413a84c1953242108442d42c8929cdb8ca31c
Dayne Broderson <dayne@alaska.edu>
2016-10-24 22:25:34 -0800

    updating apt - ensure converge on ubuntu16.04

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,11 @@ maintainer_email 'j@p4nt5.com'
 license          'apache2'
 description      'Installs/Configures Steam'
 long_description 'Installs/Configures Steam'
-version          '2.1.1'
+version          '2.2.1'
 
 depends          'dmg', '~> 2.2'
 depends          'windows', '~> 1.37'
-depends          'apt', '~> 3.0'
+depends          'apt', '~> 5.0'
 
 supports         'mac_os_x'
 supports         'windows'
